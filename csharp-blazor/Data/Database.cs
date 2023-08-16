77d2b4bc7323c6ce5829f87620307d87c1accde8
Marcio Raimo <marcio.raimo@loopix.com.br>
2023-08-16 16:20:46 -0300

    Projeto com C# e Blazor Server App

---
+++ csharp-blazor/Data/Database.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.JSInterop;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace csharp_blazor.Data
+{
+	public class Database
+	{
+		public static List<InputData> readFile()
+		{
+			string path = Path.Combine(Directory.GetCurrentDirectory(), "Data", "database.json");
+			string file = File.ReadAllText(path);
+			return JsonSerializer.Deserialize<List<InputData>>(file);
+		}
+
+		public static void writeFile(List<InputData> data, IJSRuntime js)
+		{
+			foreach (InputData input in data)
+			{
+				js.InvokeVoidAsync("console.log", "Meu pau de asa 2");
+				js.InvokeVoidAsync("console.log", input.email);
+				js.InvokeVoidAsync("console.log", input.password);
+			}
+			string path = Path.Combine(Directory.GetCurrentDirectory(), "Data", "database.json");
+			string toWrite = JsonSerializer.Serialize(data);
+			File.WriteAllText(path, toWrite);
+		}
+	}
+}
---
+++ csharp-blazor/Data/InputData.cs
@@ -0,0 +1,14 @@
+﻿namespace csharp_blazor.Data
+{
+    public class InputData
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public InputData()
+        {
+            email = "";
+            password = "";
+        }
+    }
+}
